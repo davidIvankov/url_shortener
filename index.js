5b2c8935b6591e1beaf95ddb45d8e2f0ae6a5387
davidIvankov <david.ivankov96@gmail.com>
2023-01-10 17:34:12 +0100

    fix: regex to chack valid url

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const dns = require('dns')
 
 const app = express();
 const bodyParser = require('body-parser');
-app.use(bodyParser.urlencoded({ extended: false}))
+app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json());
 let urls = [];
 
@@ -16,42 +16,45 @@ app.use(cors());
 
 app.use('/public', express.static(`${process.cwd()}/public`));
 
-app.get('/', function(req, res) {
+app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
 // Your first API endpoint
-app.get('/api/hello', function(req, res) {
+app.get('/api/hello', function (req, res) {
   res.json({ greeting: 'hello API' });
 });
-app.post('/api/shorturl', function(req, res){
-  let regex = /^(https?:\/\/)/
-  let original = req.body.url
-  dns.lookup(original.replace(regex, ""), function(err, address, family){
-    if (err){ res.json({
-error: 'invalid url'
-    })
-  } else if (!urls.includes(original)) {
-    urls.push(original)
-  res.json({
-  original_url: original,
-  short_url: urls.indexOf(original)
- })
-} else {
-   res.json({
-  original_url: original,
-  short_url: urls.indexOf(original)
- })
-}
+app.post('/api/shorturl', function (req, res) {
+  let regex = /^(https:\/\/)/
+  let regel = /\/\w+/g
+  let ori = req.body.url
+  let original = ori.replace(regex, "")
+  dns.lookup(original.replace(regel, ""), function (err, address, family) {
+    if (err) {
+      res.json({
+        error: 'invalid url'
+      })
+    } else if (!urls.includes(ori)) {
+      urls.push(ori)
+      res.json({
+        original_url: ori,
+        short_url: urls.indexOf(ori)
+      })
+    } else {
+      res.json({
+        original_url: ori,
+        short_url: urls.indexOf(ori)
+      })
+    }
   })
- 
- 
+
+
 })
-app.listen(port, function() {
+app.listen(port, function () {
   console.log(`Listening on port ${port}`);
 });
 
-app.get('/api/shorturl/:short?', function(req, res){
- let url = urls[req.params.short]
-res.redirect(url)
+app.get('/api/shorturl/:short?', function (req, res) {
+  let url = urls[req.params.short]
+  res.redirect(url)
 })
