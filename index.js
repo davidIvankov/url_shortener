d96838089a05583e5811257929c7403de5f7fd78
davidIvankov <david.ivankov96@gmail.com>
2023-01-13 18:26:46 +0100

    feat: added mongodb

--- index.js
+++ index.js
@@ -2,13 +2,21 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const dns = require('dns')
-
+const mongoose = require('mongoose')
+const AutoIncrement = require('mongoose-sequence')(mongoose);
 const app = express();
 const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json());
-let urls = [];
 
+mongoose.connect(process.env.MONGO_URI,  { useNewUrlParser: true, useUnifiedTopology: true })
+// auto-increment
+UrlSchema = mongoose.Schema({
+  url: String
+});
+
+Urls = mongoose.model('Urls', UrlSchema)
+UrlSchema.plugin(AutoIncrement, {inc_field: 'id'})
 // Basic Configuration
 const port = process.env.PORT || 3000;
 
@@ -26,35 +34,69 @@ app.get('/api/hello', function (req, res) {
 });
 app.post('/api/shorturl', function (req, res) {
   let regex = /^(https?:\/\/)/
-  let regel = /\/.+/g
-  let ori = req.body.url
+
+  let regel = /\/.*/g
+  let reg = /\/$/
+  let or = req.body.url
+  let ori = or.replace(reg, "")
   let original = ori.replace(regex, "")
+  if (!regex.test(or)){
+    res.json({
+      error:"Invalid URL"
+    })
+  }
+  else {
+  
   dns.lookup(original.replace(regel, ""), function (err, address, family) {
     if (err) {
       res.json({
-        error: 'invalid url'
-      })
-    } else if (!urls.includes(ori)) {
-      urls.push(ori)
-      res.json({
-        original_url: ori,
-        short_url: urls.indexOf(ori)
-      })
-    } else {
-      res.json({
-        original_url: ori,
-        short_url: urls.indexOf(ori)
+        error: 'Invalid URL'
       })
     }
+      else {
+    Urls.exists({url: or}, function(err, data){
+      if (err) console.error(err)
+      if (data === false){
+        let UrlInsert = new Urls({
+          url: or
+        })
+UrlInsert.save()
+setTimeout( function(){
+  Urls.findOne({url: or},'_id', function(err, data){
+  if (err) console.error(err)
+  else res.json({
+    original_url: or,
+    short_url: data._id
   })
-
-
+})}, 1000)
+      } else {
+console.log('exsists')
+Urls.findOne({url: or},'_id', function(err, data){
+  if (err) console.error(err)
+  else res.json({
+    original_url: or,
+    short_url: data._id
+  })
 })
+      }
+    })
+  
+    
+  
+}
+  })
+}
+})
+
+
 app.listen(port, function () {
   console.log(`Listening on port ${port}`);
 });
 
-app.get('/api/shorturl/:short?', function (req, res) {
-  let url = urls[req.params.short]
-  res.redirect(url)
+app.get('/api/shorturl/:url', function (req, res) {
+  
+ Urls.findOne({_id:req.params.url},'url', function(err, data){
+  if(err) console.error(err)
+  res.redirect(data.url)
+ })
 })
